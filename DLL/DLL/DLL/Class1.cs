55d7d10ca7a4968b76bb7ab3566d4f0a52563d1c
PHINX <82601259+PHINXED@users.noreply.github.com>
2022-02-03 16:37:30 +0100

    CREACION DE UNA DLL DE POWERSHELL

---
+++ DLL/DLL/DLL/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Management.Automation;  // Windows PowerShell namespace.
+
+namespace PowerSh
+{
+    public class PowerSh
+    {
+        public static void CrearUsuario()
+        {
+
+            //Create a PowerShell object.
+            PowerShell ps = PowerShell.Create();
+
+            //New-LocalUser usuario -Password (ConvertTo-SecureString "11234aaadsf" -asplaintext -force)
+            ps.AddScript("New-LocalUser usuario -Password (ConvertTo-SecureString '11234aaadsf' -asplaintext -force)");
+
+            IAsyncResult result = ps.BeginInvoke();
+
+            // do something else until execution has completed.
+            // this could be sleep/wait, or perhaps some other work
+            while (result.IsCompleted == false)
+            {
+            }
+
+        } // End Main.
+        public static void ListarUsuario()
+        {
+
+            //Create a PowerShell object.
+            PowerShell ps = PowerShell.Create();
+
+            //New-LocalUser usuario -Password (ConvertTo-SecureString "11234aaadsf" -asplaintext -force)
+            ps.AddScript("Get-LocalUser");
+
+            IAsyncResult result = ps.BeginInvoke();
+
+            // do something else until execution has completed.
+            // this could be sleep/wait, or perhaps some other work
+            while (result.IsCompleted == false)
+            {
+            }
+            foreach (PSObject result2 in ps.EndInvoke(result))
+            {
+                Console.WriteLine("{0}",
+                        result2.Members["Name"].Value);
+            } // End foreach.
+            System.Console.WriteLine("Hit any key to exit.");
+        } // End Main.
+    } // End HostPs1.
+}
